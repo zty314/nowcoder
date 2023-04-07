9ae11fd8d9e052d911bd8a4347900a33e71826b3
zty314 <maayasakamoto@sina.cn>
2023-04-07 22:17:33 +0800

    backTrace

---
+++ src/main/java/com/example/offer/backtrace/OfferII082.java
@@ -0,0 +1,40 @@
+package com.example.offer.backtrace;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class OfferII082 {
+
+    List<List<Integer>> ans = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    int currSum = 0;
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        backTrace(candidates, target, 0);
+        return ans;
+    }
+
+    public void backTrace(int[] candidates, int target, int start) {
+        if (currSum == target) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        if(currSum > target) {
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            currSum = currSum + candidates[i];
+            path.add(candidates[i]);
+            backTrace(candidates, target, i + 1);
+            path.removeLast();
+            currSum = currSum - candidates[i];
+        }
+    }
+}
---
+++ src/main/java/com/example/offer/backtrace/OfferII084.java
@@ -0,0 +1,40 @@
+package com.example.offer.backtrace;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class OfferII084 {
+
+    List<List<Integer>> ans = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Arrays.sort(nums);
+        boolean[] used = new boolean[nums.length];
+        backTrace(nums, used);
+        return ans;
+    }
+
+    public void backTrace(int[] nums, boolean[] used) {
+        if (nums.length == path.size()) {
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            backTrace(nums, used);
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+}
